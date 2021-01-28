ff0ab2375ade9ba7861810c8df8a1348386cbcaa
1lxrd <54149933+1lxrd@users.noreply.github.com>
2021-01-28 14:51:41 +0200

    Add files via upload

---
+++ lesson_1/picture.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "(**)" << endl;
+	cout << "(^)" << endl;
+	cout << " _" << endl;
+	
+	system("pause");
+}
